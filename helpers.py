1e37d7d706f3c1077b64bf1aafbd364a9d06bdc8
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2018-06-20 23:00:47 +0100

    Taking log of Sobolev opacity equation. opacity = logtau * (float(vref) - v) / aux

--- helpers.py
+++ helpers.py
@@ -3,6 +3,6 @@ import numpy as np
 
 def calc_opacity(v, logtau, vref, aux):
     """ Calculate Cobolev Opacity as in eq 2 of http://iopscience.iop.org/article/10.1086/658673/pdf"""
-    opacity = logtau * np.exp((float(vref) - v) / aux)
+    opacity = logtau * (float(vref) - v) / aux
 
     return opacity
